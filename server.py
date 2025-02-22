886c6fcbbf6d0513a56a898bb6fc6daf49f88fb0
Lindy <lindyjchen@gmail.com>
2025-02-22 10:58:01 -0600

    flask venv setup

--- server.py
+++ server.py
@@ -1,22 +1,21 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
-
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
 
-# main driver function
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+
 if __name__ == '__main__':
-
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    app.run()
+    app.run(debug=True)
