5f44b1f3def4f927d028b9a97d12da0f4f296678
MousyYaya <Mousy9231@gmail.com>
2025-02-23 08:42:52 -0600

    added the graph line sand put the ids through the pages

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import folium
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from backend.generatePaths import GeneratePaths
+from dotenv import load_dotenv
+import logging
+
+#  app.logger.debug(f"{var}")  # Debugging line
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # 3 fslash = relative path, 4 is absolute. We want this to reside in project location
@@ -54,7 +58,6 @@ def index():
         events = Event.query.order_by(Event.startTime).all()
         return render_template('index.html', events=events)
 
-
 @app.route('/delete/<int:id>')
 def delete(id):
     event_to_delete = Event.query.get_or_404(id)
@@ -96,15 +99,38 @@ def update(id):
     else:
         return render_template('update.html', event=event)
 
-@app.route('/map', methods=["GET", "POST"])
-def map():
+@app.route('/map/<int:id>', methods=["GET", "POST"])
+def map(id):
+    addresses = []
+    checked = []
+
+    # getting start and end locations
+    events = Event.query.order_by(Event.startTime).all()
+    index = events.index(Event.query.get_or_404(id))
+    start_location = events[index].location
+    end_location = events[index + 1].location
 
     if request.method == "POST": 
-        checked = request.form.getlist("checked")
+        
+        checkedAll = request.form.getlist("checked")
+        checked.append(start_location)
+        addresses.append(start_location)
+
+        app.logger.debug(f"{checkedAll}")
+
+        for item in checkedAll:
+            location, address = item.split(' - ')
+            checked.append(location)
+            addresses.append(address)
+        checked.append(end_location)
+        addresses.append(end_location)
+
 
         #Create map object
         mapObj = folium.Map(location=[43.0757, -89.4040],
                         zoom_start=15, width=800, height=500)
+        
+        waypoints = []
 
         #for each location add a marker to map object 
         for check in checked: 
@@ -112,16 +138,24 @@ def map():
             name = check
             folium.Marker(coords, 
                         popup=name).add_to(mapObj)
-        #render map obj
+            waypoints.append(coords)
+
+        shortestPath = backend.getShortestPath(addresses)
+
+        for index, route in enumerate(shortestPath):
+            color = "red" if index % 2 == 0 else "black"
+            folium.PolyLine(route, color=color, weight=5, opacity=0.7).add_to(mapObj)
+
+        # render map obj
         mapObj.get_root().render()
         
         #derive script and style tags to be rendered in HTML head
         header = mapObj.get_root().header.render()
 
-        #derive the div container to be rendered in the HTML body
+        # derive the div container to be rendered in the HTML body
         body_html = mapObj.get_root().html.render()
 
-        #save as HTML file
+        # save as HTML file
         mapObj.save("./templates/output.html")
 
         return render_template("output.html")
@@ -129,10 +163,9 @@ def map():
     else:
         return "Sorry, you have to check something"
     
-
 @app.route('/study_spots/<int:id>')
 def study_spots(id):
-    #use backend func to get list of libraries 
+    # use backend func to get list of libraries 
     events = Event.query.order_by(Event.startTime).all()
 
     index = events.index(Event.query.get_or_404(id))
@@ -141,26 +174,26 @@ def study_spots(id):
         return "Go anywhere: the world is your oyster"
     
     else: 
-        temp = backend.generateNearByDict(events[index].location, events[index + 1].location, "library", 500) #500m = 7 min walk
-        locations=list(temp.keys())
-        return render_template('study_spots.html', locations=locations)
+        locations = backend.generateNearByDict(events[index].location, events[index + 1].location, "library", 500) #500m = 7 min walk
+
+        return render_template('study_spots.html', locations=locations, id=id)
 
 @app.route('/food_spots/<int:id>')
 def food_spots(id):
 
-    #use backend func to get list of restaurants  
+    # use backend func to get list of restaurants  
     events = Event.query.order_by(Event.startTime).all()
     index = events.index(Event.query.get_or_404(id))
 
-    #if event is last in schedule 
+    # if event is last in schedule 
     if index == len(events) - 1: 
         return "Go anywhere: the world is your oyster"
     
     else: 
-        temp = backend.generateNearByDict(events[index].location, events[index + 1].location, "restaurant", 500) #500m = 7 min walk
-        locations=list(temp.keys())
+        locations = backend.generateNearByDict(events[index].location, events[index + 1].location, "restaurant", 500) #500m = 7 min walk
+        
 
-    return render_template('food_spots.html', locations=locations)
+    return render_template('food_spots.html', locations=locations, id=id)
 
 if __name__ == '__main__':
     app.run(debug=True)
